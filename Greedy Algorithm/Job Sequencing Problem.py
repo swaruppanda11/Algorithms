fe59a1fa7a7596d2e5d8b62530a410845b349d62
Swarup Panda <67518032+swaruppanda11@users.noreply.github.com>
2023-05-08 23:46:22 +0530

    Initial Commit

---
+++ Greedy Algorithm/Job Sequencing Problem.py
@@ -0,0 +1,42 @@
+import heapq
+
+def printJobScheduling(arr):
+	n = len(arr)
+	arr.sort(key=lambda x: x[1])
+	result = []
+	maxHeap = []
+
+	for i in range(n - 1, -1, -1):
+
+		if i == 0:
+			slots_available = arr[i][1]
+		else:
+			slots_available = arr[i][1] - arr[i - 1][1]
+
+		heapq.heappush(maxHeap, (-arr[i][2], arr[i][1], arr[i][0]))
+
+		while slots_available and maxHeap:
+
+			profit, deadline, job_id = heapq.heappop(maxHeap)
+
+			slots_available -= 1
+
+			result.append([job_id, deadline])
+
+	result.sort(key=lambda x: x[1])
+
+	for job in result:
+		print(job[0], end=" ")
+	print()
+
+
+if __name__ == '__main__':
+	arr = [['a', 2, 100], 
+		['b', 1, 19],
+		['c', 2, 27],
+		['d', 1, 25],
+		['e', 3, 15]]
+
+	print("Following is maximum profit sequence of jobs")
+
+	printJobScheduling(arr)
